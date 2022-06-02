c0bf5b467e9e0752c84b4e5d701a2518cb484709
jieun0411 <annakim0411@naver.com>
2022-06-02 16:11:52 +0900

    Custom Hook Practice

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { useState } from "react";
-import Board from "./components/Board";
-import Pagination from "./components/Pagination";
+import Checkbox from "./components/Checkbox";
+import useToggle from "./hooks/useToggle";
+import Box from "./components/Box";
+import useHover from "./hooks/useHover";
+import useKeyPress from "./hooks/useKeyPress";
 
 function App() {
-  const [page, setPage] = useState(0);
-  // dummy data
-  const articles = new Array(100).fill().map((_, i) => ({
-    id: i,
-    title: `${i}번 게시물`,
-  }));
+  const [on, toggle] = useToggle();
+  const [ref, isHover] = useHover();
+  const keyPressed = useKeyPress("a");
 
-  const limit = 6; // 페이지 당 보여지는 게시물 갯수
-  const offset = page * limit;
-
+  console.log(on);
   return (
     <div>
-      <Pagination
-        defaultPage={0}
-        limit={10}
-        total={articles.length}
-        onChange={setPage}
-      />
-      <Board articles={articles.slice(offset, offset + limit)} />
+      {/* <button onClick={toggle}>{on ? "True" : "False"}</button> */}
+      <Checkbox checked={on} onChange={toggle} />
+
+      {isHover ? "hover" : "mouseout"}
+      <Box ref={ref} />
+
+      {keyPressed && "Pressed"}
     </div>
   );
 }
