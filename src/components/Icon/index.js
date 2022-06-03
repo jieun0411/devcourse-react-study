60cc71e1289c0ffee59411e8f01ad4e45574f769
jieun0411 <annakim0411@naver.com>
2022-06-03 22:03:23 +0900

    컴포넌트 연습하기-Icon

---
+++ src/components/Icon/index.js
@@ -0,0 +1,38 @@
+import { Buffer } from "buffer";
+import styled from "@emotion/styled";
+
+const IconWrapper = styled.i`
+  display: inline-black;
+`;
+
+const Icon = ({
+  name,
+  size = 16,
+  strokeWidth = 2,
+  rotate,
+  color = "#222",
+  ...props
+}) => {
+  const shapeStyle = {
+    width: size,
+    height: size,
+    transform: rotate ? `rotate(${rotate}deg)` : undefined,
+  };
+  const iconStyle = {
+    "stroke-width": strokeWidth,
+    stroke: color,
+    width: size,
+    height: size,
+  };
+  const icon = require("feather-icons").icons[name];
+  const svg = icon ? icon.toSvg(iconStyle) : "";
+  const base64 = Buffer.from(svg, "utf8").toString("base64"); // img 태그를 이용하기 위해 base64로 변환
+
+  return (
+    <IconWrapper {...props} style={shapeStyle}>
+      <img src={`data:image/svg+xml;base64,${base64}`} alt={name} />
+    </IconWrapper>
+  );
+};
+
+export default Icon;
