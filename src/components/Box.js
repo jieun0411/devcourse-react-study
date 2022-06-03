f2842549a5a2828ea6104c487a98a941f0f2bd9c
jieun0411 <annakim0411@naver.com>
2022-06-03 16:42:07 +0900

    컴포넌트 연습하기 (누적 코드 불러오기, 폴더명 수정)

--- src/components/Box.js
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: blue;
-`;
-
-export default Box;
---
+++ src/stories/Spinner.stories.js
@@ -0,0 +1,18 @@
+import Spinner from "../components/Spinner";
+
+export default {
+  title: "Component/Spinner",
+  component: Spinner,
+  argTypes: {
+    size: {
+      defaultValues: 24,
+      control: "number",
+    },
+    color: { control: "color" },
+    loading: { control: "boolean" },
+  },
+};
+
+export const Default = (args) => {
+  return <Spinner {...args} />;
+};
