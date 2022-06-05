48fa84e6945d889601cabe3592dbdc1aec7651a7
jieun0411 <annakim0411@naver.com>
2022-06-05 20:06:36 +0900

    컴포넌트 연습하기-Flux

---
+++ src/stories/Flux.stories.js
@@ -0,0 +1,63 @@
+import Flux from "../components/Flux";
+
+const { Row, Col } = Flux;
+
+export default {
+  title: "Component/Flux",
+  component: Flux,
+  argTypes: {},
+};
+
+const Box = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#44b",
+        width: "100%",
+        height: 18,
+        color: "white",
+        textAlign: "center",
+        borderRadius: 8,
+      }}
+    >
+      Box
+    </div>
+  );
+};
+
+export const Default = (args) => {
+  return (
+    <Row gutter={[8, 8]}>
+      <Col span={4}>
+        <Box />
+      </Col>
+      <Col span={2}>
+        <Box />
+      </Col>
+      <Col span={2}>
+        <Box />
+      </Col>
+      <Col span={2}>
+        <Box />
+      </Col>
+      <Col span={2}>
+        <Box />
+      </Col>
+      <Col span={4}>
+        <Box />
+      </Col>
+      <Col span={4}>
+        <Box />
+      </Col>
+      <Col span={4}>
+        <Box />
+      </Col>
+      <Col offset={4} span={8}>
+        <Box />
+      </Col>
+      <Col span={12}>
+        <Box />
+      </Col>
+    </Row>
+  );
+};
