d713f476d75cc701f9a4b29ed48ea3ba22fe366f
jieun0411 <annakim0411@naver.com>
2022-06-05 20:33:15 +0900

    컴포넌트 연습하기-Breadcrumb

---
+++ src/stories/Breadcrumb.stories.js
@@ -0,0 +1,16 @@
+import Breadcrumb from "../components/Breadcrumb";
+
+export default {
+  title: "Component/Breadcrumb",
+  component: Breadcrumb,
+  argTypes: {},
+};
+export const Default = (args) => {
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item href="/home">Home</Breadcrumb.Item>
+      <Breadcrumb.Item href="/level1">Level 1</Breadcrumb.Item>
+      <Breadcrumb.Item>Level 2</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
