8ff03de7ed727b52395290feaa5d7b35bdb87a5e
jieun0411 <annakim0411@naver.com>
2022-06-06 12:27:24 +0900

    사용자 정의 Hook 연습하기-useHover

---
+++ src/stories/hooks/useHover.stories.js
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+import useHover from "../../hooks/useHover";
+
+export default {
+  title: "Hook/useHover",
+  component: useHover,
+  argTypes: {},
+};
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`;
+
+export const Default = () => {
+  const [ref, hover] = useHover();
+
+  return (
+    <>
+      <Box ref={ref} />
+      {hover ? "True" : "False"}
+    </>
+  );
+};
